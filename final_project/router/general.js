113d43ba5a020b655f40df017950c86761984f6a
“AdamC” <415744211@qq.com>
2023-08-22 03:46:15 -0400

    Promises/Async in general.js updated
    Signed-off-by: “AdamC” <“415744211@qq.com”>

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,6 +2,7 @@ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
+const axios = require('axios');
 const public_users = express.Router();
 
 
@@ -21,13 +22,39 @@ public_users.post("/register", (req,res) => {
 public_users.get('/',function (req, res) {
   res.status(201).json(books)
 });
+//Get all books using Async callbacks
+public_users.get("/server/asynbooks", async function (req,res) {
+    try {
+      let response = await axios.get("http://localhost:5000/");
+      console.log(response.data);
+      return res.status(200).json(response.data);
+      
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({message: "Error getting book list"});
+    }
+  });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   let isbn = req.params.isbn;
   res.status(201).json(books[isbn]);
  });
-  
+ //Get book details by ISBN using Promises
+ public_users.get("/server/asynbooks/isbn/:isbn", function (req,res) {
+    let {isbn} = req.params;
+    axios.get(`http://localhost:5000/isbn/${isbn}`)
+    .then(function(response){
+      console.log(response.data);
+      return res.status(200).json(response.data);
+    })
+    .catch(function(error){
+        console.log(error);
+        return res.status(500).json({message: "Error while fetching book details."})
+    })
+  });
+
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
@@ -46,6 +73,19 @@ public_users.get('/author/:author',function (req, res) {
     res.status(404).send('No books found for author');  
   }
 });
+//Get book details by author using promises
+public_users.get("/server/asynbooks/author/:author", function (req,res) {
+    let {author} = req.params;
+    axios.get(`http://localhost:5000/author/${author}`)
+    .then(function(response){
+      console.log(response.data);
+      return res.status(200).json(response.data);
+    })
+    .catch(function(error){
+        console.log(error);
+        return res.status(500).json({message: "Error while fetching book details."})
+    })
+  });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
@@ -58,8 +98,23 @@ public_users.get('/title/:title',function (req, res) {
   else{
       return res.status(404).json({message: "Book not found"});
   }
-//return res.status(300).json({message: "Yet to be implemented"});
 });
+
+
+//Get all books based on title using promises
+public_users.get("/server/asynbooks/title/:title", function (req,res) {
+    let {title} = req.params;
+    axios.get(`http://localhost:5000/title/${title}`)
+    .then(function(response){
+      console.log(response.data);
+      return res.status(200).json(response.data);
+    })
+    .catch(function(error){
+        console.log(error);
+        return res.status(500).json({message: "Error while fetching book details."})
+    })
+  });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
